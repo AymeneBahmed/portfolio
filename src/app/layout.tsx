75f2270f3770d590ff51d08f3f39b9cea4130306
The cool news guy <joewilliamdd31@gmail.com>
2024-10-03 07:34:55 +0100

    Create Navbar with links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "../../providers/ThemeProvider";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
           defaultTheme="dark"
           disableTransitionOnChange
         >
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  const navLinks = ["about me", "projects", "experience", "skills", "contact"];
+
+  return (
+    <nav className="flex h-[4.5rem] items-center justify-center border-b bg-muted/40">
+      <div className="flex w-[40%] cursor-pointer justify-between">
+        {navLinks.map((link) => (
+          <Link
+            key={link}
+            className="group relative font-semibold capitalize underline-offset-8 transition-colors duration-300 hover:text-primary"
+            href="/"
+          >
+            <div className="absolute bottom-[-3px] left-0 h-0.5 w-0 rounded-full bg-primary transition-[width] group-hover:w-full"></div>
+            {link}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
+}
