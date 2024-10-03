ce34013d0a2672b3c2bf3feee510f96b27f37682
The cool news guy <joewilliamdd31@gmail.com>
2024-10-03 19:16:39 +0100

    Create VerticalNavbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,37 @@
+"use client";
+
 import Link from "next/link";
 import ThemeTogglerButton from "./ThemeTogglerButton";
 import { Url } from "next/dist/shared/lib/router/router";
 import { cn } from "@/lib/utils";
+import { createPortal } from "react-dom";
+import { X } from "lucide-react";
+import { useState } from "react";
+
+const navLinks = ["about me", "projects", "experience", "skills", "contact"];
 
 export default function Navbar() {
-  const navLinks = ["about me", "projects", "experience", "skills", "contact"];
+  const [shouldShowVerticalNavbar, setShouldShowVerticalNavbar] =
+    useState(false);
 
+  // ! 830px
   return (
     <nav className="relative flex h-[4.5rem] items-center justify-center border-b bg-muted/40">
-      <div className="flex w-[40%] cursor-pointer justify-between">
+      <button
+        className="invisible absolute left-10 space-y-1.5 max-[830px]:visible"
+        onClick={() => setShouldShowVerticalNavbar(true)}
+      >
+        {[...Array(3)].map((_, i) => (
+          <div key={i} className="h-1 w-8 rounded-full bg-primary"></div>
+        ))}
+      </button>
+
+      <VerticalNavbar
+        shouldShowVerticalNavbar={shouldShowVerticalNavbar}
+        setShouldShowVerticalNavbar={setShouldShowVerticalNavbar}
+      />
+
+      <div className="flex w-[max(40%,480px)] cursor-pointer flex-nowrap justify-between max-[830px]:hidden">
         {navLinks.map((link) => (
           <NavLink key={link} href={"/"} text={link} />
         ))}
@@ -16,6 +39,44 @@ export default function Navbar() {
 
       <ThemeTogglerButton className="absolute right-10" />
     </nav>
+  );
+}
+
+function VerticalNavbar({
+  shouldShowVerticalNavbar,
+  setShouldShowVerticalNavbar,
+}: {
+  shouldShowVerticalNavbar: boolean;
+  setShouldShowVerticalNavbar: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  return createPortal(
+    <div
+      className={cn(
+        "absolute left-0 top-0 z-[9999] grid size-full place-content-center bg-white/50 opacity-0 backdrop-blur-sm transition-all duration-500 dark:bg-black/50 min-[830px]:hidden",
+        !shouldShowVerticalNavbar && "pointer-events-none",
+        shouldShowVerticalNavbar && "opacity-100",
+      )}
+    >
+      <button
+        className="absolute right-4 top-4 transition-colors duration-300 hover:text-primary"
+        onClick={() => setShouldShowVerticalNavbar(false)}
+      >
+        <X size={50} />
+      </button>
+
+      <div className="space-y-8 text-center">
+        {navLinks.map((link) => (
+          <button
+            key={link}
+            className="block w-fit text-4xl"
+            onClick={() => setShouldShowVerticalNavbar(false)}
+          >
+            <NavLink href="/" text={link} />
+          </button>
+        ))}
+      </div>
+    </div>,
+    document.body,
   );
 }
 
