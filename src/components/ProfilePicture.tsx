a262f904279df8e9d571470e3d8f94e8dbc83a06
The cool news guy <joewilliamdd31@gmail.com>
2024-10-04 20:47:43 +0100

    Make ProfilePicture a server component

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import profilePicture from "@/assets/profile picture.jpeg";
 import { cn } from "@/lib/utils";
-import { useTheme } from "next-themes";
 import Image from "next/image";
 
 interface ProfilePictureProps {
@@ -10,16 +7,9 @@ interface ProfilePictureProps {
 }
 
 export default function ProfilePicture({ className }: ProfilePictureProps) {
-  const { theme } = useTheme();
-
   return (
     <div className={cn("relative size-[18rem] rounded-full", className)}>
-      <div
-        className="absolute left-0 top-0 size-full scale-[1.03] rounded-full bg-primary"
-        style={{
-          boxShadow: `0 0 15px 0px ${theme === "dark" ? "hsl(180 100% 50%)" : "hsl(221.2 83.2% 53.3%)"}`,
-        }}
-      ></div>
+      <div className="absolute left-0 top-0 size-full scale-[1.03] rounded-full bg-primary shadow-[0_0_15px_0px] shadow-primary"></div>
 
       <Image
         src={profilePicture}
