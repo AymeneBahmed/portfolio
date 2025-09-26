b0ac9f24204f6ef696f841a8593d21421045a88d
Aymene Bahmed <aymendd3131@gmail.com>
2025-09-26 12:51:45 +0100

    Change the year to 2025 in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <section className="flex h-14 items-center justify-center border-t font-semibold tracking-wide">
-      Copyright &copy; 2024 Aymene Bahmed
+      Copyright &copy; 2025 Aymene Bahmed
     </section>
   );
 }
