fa27f0fa8191a6297b1c685506ef7a43c35c75f5
Aymene <aymendd3131@gmail.com>
2026-07-08 23:06:01 +0100

    Make Footer's year dynamic

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <section className="flex h-14 items-center justify-center border-t font-semibold tracking-wide">
-      Copyright &copy; 2025 Aymene Bahmed
+      Copyright &copy; {currentYear} Aymene Bahmed
     </section>
   );
 }
