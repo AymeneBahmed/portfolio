2ae16991f992a5ab95b4b17d32a66587aa52d49e
Aymene <aymendd3131@gmail.com>
2026-07-11 15:20:27 +0100

    Add priority to ProfilePicture

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -18,9 +18,10 @@ export default function ProfilePicture({ className }: ProfilePictureProps) {
       <Image
         src={profilePicture}
         alt="profile picture"
-        fill
         className="scale-[1.2] rounded-full object-cover object-[110%_-30px]"
         loading="eager"
+        priority
+        fill
       />
     </Tilt>
   );
