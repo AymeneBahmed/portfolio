cc9cb04b8d2d027c96d19d21bcfeb07916731b55
Aymene <aymendd3131@gmail.com>
2026-07-11 06:41:19 +0100

    Use footer tag for Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <section className="flex h-14 items-center justify-center border-t font-semibold tracking-wide">
+    <footer className="flex h-14 items-center justify-center border-t font-semibold tracking-wide">
       Copyright &copy; {currentYear} Aymene Bahmed
-    </section>
+    </footer>
   );
 }
