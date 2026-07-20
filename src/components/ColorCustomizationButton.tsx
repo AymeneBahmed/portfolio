4edf4e1f13f3c7a10320cf925edb163293f4e4d5
Aymene <aymendd3131@gmail.com>
2026-07-20 16:33:20 +0100

    Add light color scheme support

--- src/components/ColorCustomizationButton.tsx
+++ src/components/ColorCustomizationButton.tsx
@@ -5,12 +5,47 @@ import { Button } from "./ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { useState, useEffect } from "react";
 import { cn } from "@/lib/utils";
-import { useTheme } from "next-themes";
+import { useTheme as useColor } from "next-themes";
 import { COLOR_THEMES } from "@/lib/constants";
 
 export default function ColorCustomizationButton() {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme } = useColor();
   const [isShuffling, setIsShuffling] = useState<boolean>(false);
+  const [currentColorScheme, setCurrentColorScheme] = useState<
+    "light" | "dark" | null
+  >();
+  const currentColorSchemeColors = COLOR_THEMES.filter((themeName) =>
+    currentColorScheme === "dark"
+      ? themeName.startsWith("dark")
+      : !themeName.startsWith("dark"),
+  );
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme")?.startsWith("dark")
+      ? "dark"
+      : "light";
+
+    setCurrentColorScheme(storedTheme);
+  }, []);
+
+  useEffect(() => {
+    if (!theme) {
+      return;
+    }
+
+    // Store the color of current theme so changing between themes uses the old theme's color
+    if (currentColorScheme === "light") {
+      localStorage.setItem("last-used-light-theme", theme);
+      document.documentElement.classList.remove("dark");
+
+      return;
+    }
+
+    if (currentColorScheme === "dark") {
+      localStorage.setItem("last-used-dark-theme", theme);
+      document.documentElement.classList.add("dark");
+    }
+  }, [currentColorScheme, theme]);
 
   useEffect(() => {
     const shouldShuffle = localStorage.getItem("shuffle");
@@ -37,9 +72,9 @@ export default function ColorCustomizationButton() {
     return () => clearInterval(interval);
   }, [isShuffling, setTheme]);
 
-  function handleThemeSelect(themeName: string) {
+  function handleColorSelect(colorName: string) {
     setIsShuffling(false);
-    setTheme(themeName);
+    setTheme(colorName);
   }
 
   return (
@@ -63,34 +98,78 @@ export default function ColorCustomizationButton() {
           </Button>
         </PopoverTrigger>
         <PopoverContent>
-          <div className="text-sm">Colors</div>
-          <hr className="border-border my-2" />
-          <div className="flex flex-wrap gap-1.5">
-            {COLOR_THEMES.map((themeName) => (
+          <div>
+            <div className="text-sm">Color schemes</div>
+            <hr className="border-border my-2" />
+            <div className="flex flex-wrap gap-1.5">
               <Button
-                key={themeName}
                 size="icon"
                 className={cn(
-                  `border-secondary ${themeName} relative size-10 cursor-pointer rounded-full border-2`,
-                  !isShuffling &&
-                    theme === themeName &&
+                  `border-secondary relative size-10 cursor-pointer rounded-full border-2 bg-white transition-opacity hover:bg-white hover:opacity-85`,
+                  currentColorScheme === "light" &&
                     "ring-ring ring-2 ring-offset-2",
                 )}
-                onClick={() => handleThemeSelect(themeName)}
-                aria-label={`Use a ${themeName} theme`}
+                onClick={() => {
+                  setCurrentColorScheme("light");
+
+                  const storedColor = localStorage.getItem(
+                    "last-used-light-theme",
+                  );
+
+                  setTheme(storedColor ?? currentColorSchemeColors[0]);
+                }}
+                aria-label="Use light color scheme"
               />
-            ))}
-            <Button
-              size="icon"
-              className={cn(
-                "border-secondary hover:bg-muted bg-muted relative size-10 cursor-pointer rounded-full border-2",
-                isShuffling && "ring-ring ring-2 ring-offset-2",
-              )}
-              onClick={() => setIsShuffling((prev) => !prev)}
-              aria-label="Shuffle themes"
-            >
-              <ShuffleIcon className="stroke-muted-foreground" size={15} />
-            </Button>
+              <Button
+                size="icon"
+                className={cn(
+                  `border-secondary relative size-10 cursor-pointer rounded-full border-2 bg-[oklch(0.208_0.04_265.731)] hover:bg-[oklch(0.208_0.04_265.731)] hover:opacity-85`,
+                  currentColorScheme === "dark" &&
+                    "ring-ring ring-2 ring-offset-2",
+                )}
+                onClick={() => {
+                  setCurrentColorScheme("dark");
+
+                  const storedColor = localStorage.getItem(
+                    "last-used-dark-theme",
+                  );
+
+                  setTheme(storedColor ?? currentColorSchemeColors[0]);
+                }}
+                aria-label="Use dark color scheme"
+              />
+            </div>
+          </div>
+          <div className="mt-4">
+            <div className="text-sm">Colors</div>
+            <hr className="border-border my-2" />
+            <div className="flex flex-wrap gap-1.5">
+              {currentColorSchemeColors.map((colorName) => (
+                <Button
+                  key={colorName}
+                  size="icon"
+                  className={cn(
+                    `border-secondary ${colorName} relative size-10 cursor-pointer rounded-full border-2`,
+                    !isShuffling &&
+                      theme === colorName &&
+                      "ring-ring ring-2 ring-offset-2",
+                  )}
+                  onClick={() => handleColorSelect(colorName)}
+                  aria-label={`Use a ${colorName} theme`}
+                />
+              ))}
+              <Button
+                size="icon"
+                className={cn(
+                  "border-secondary hover:bg-muted bg-muted relative size-10 cursor-pointer rounded-full border-2",
+                  isShuffling && "ring-ring ring-2 ring-offset-2",
+                )}
+                onClick={() => setIsShuffling((prev) => !prev)}
+                aria-label="Shuffle themes"
+              >
+                <ShuffleIcon className="stroke-muted-foreground" size={15} />
+              </Button>
+            </div>
           </div>
         </PopoverContent>
       </Popover>
