c2f11c4a1efeb5eb8e8f710789742bca086ba387
Aymene <aymendd3131@gmail.com>
2026-07-20 19:53:16 +0100

    Make Footer support light-theme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="flex h-14 items-center justify-center border-t font-semibold tracking-wide">
+    <footer className="border-primary flex h-14 items-center justify-center border-t-2 font-semibold tracking-wide">
       Copyright &copy; {currentYear} Aymene Bahmed
     </footer>
   );
